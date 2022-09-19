0b671935ada5014e2d1ca7266906e6f73341ed81
Rayana Vital <rayanavital@gmail.com>
2022-09-19 19:14:54 -0300

    Adicionando comentários no servicel.

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -1,20 +1,53 @@
+/**
+ * Serviço responsavel por executar as operações
+ * da calculadora.
+ *
+ * @author Rayana Vital<rayanavital@gmail.com>
+ * @since 1.0.0
+ */
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculadoraService {
-
+  /* Define as constantes utilizadas para
+  indentificar as operações de calculo   */
   static readonly SOMA: string = '+';
   static readonly SUBTRACAO: string = '-';
   static readonly MULTIPLICACAO: string = '*';
   static readonly DIVISAO: string = '/';
 
   constructor() { }
+  /** Método que calcula a operação matemática dado dois numeros.
+   * Suporta as operações soma, subtração, multiplicação e divisão.
+   *
+   * @param num1
+   * @param num2
+   * @param operacao
+   * @return number resultado da operação
+  */
 
-  calcular(num1: number, num2: number, operacao: string): number {
+  calcular(num1: number, num2: number, operacao: string) {
     let result: number; //armazena o resultado da operação
 
-    
+    switch(operacao){
+      case CalculadoraService.SOMA:
+        result = num1 + num2;
+      break;
+      case CalculadoraService.SUBTRACAO:
+        result = num1 - num2;
+      break;
+      case CalculadoraService.MULTIPLICACAO:
+        result = num1 * num2;
+      break;
+      case CalculadoraService.DIVISAO:
+        result = num1 / num2;
+      break;
+      default:
+        result = 0;
+    }
+
+
   }
 }
